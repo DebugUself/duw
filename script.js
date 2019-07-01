6f2fd045574a10d83d7ab81822d46a0571257b8b
Zoom.Quiet <zoomquiet+git@gmail.com>
2019-07-01 21:45:58 +0800

    👌 IMPROVE: pub(site) build inline by Docute with inv.(at 190701 2145 58.669)

--- script.js
+++ script.js
@@ -37,6 +37,10 @@ new Docute({
       {},{title: '2019',
 links: [
 {
+                              title: '115w',
+                              link: '/115w'
+                            },
+{
                               title: '114w',
                               link: '/114w'
                             },
